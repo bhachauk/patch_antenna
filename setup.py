9c99ba22f9abb8819dc70bcd59ceed1a61d2fe2f
bhanuchander <bhanuchander210@gmail.com>
2020-06-25 09:41:38 +0530

    patch_antenna description readme added.

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='patch_antenna',
       version='0.0.1',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       description='A simple patch antenna design library',
       url='https://github.com/bhanuchander210/design_patch_antenna.git',
       author='Bhanuchander Udhayakumar',
