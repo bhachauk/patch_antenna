4b4015bc03aa0c83e031b9fff736ef57417ec3f7
bhanuchanderu <bhanuchanderu@nmsworks.co.in>
2022-04-16 17:04:33 +0530

    travis codecov added.

--- tests/test_patch_antenna.py
+++ tests/test_patch_antenna.py
@@ -24,3 +24,37 @@ def test_thickness_limit():
         pa.design(10 ** 9, 1, 0)
 
     return execinfo.value.args[0] == 'Thickness value should be in greater than 0 and smaller or equals 1 meter'
+
+
+def test_design_string():
+    freq = 2.4 * 10 ** 9
+    er = 4.4
+    h = 1.6 * 10 ** -3
+    result = pa.design_string(freq, er, h)
+    assert isinstance(result, str)
+
+
+def test_gerber():
+    freq = 2.4 * 10 ** 9
+    er = 4.4
+    h = 1.6 * 10 ** -3
+    pa.write_gerber(freq, er, h, 'test.gbr', "normal")
+    assert True
+
+
+def test_gerber2():
+    freq = 2.4 * 10 ** 9
+    er = 4.4
+    h = 1.6 * 10 ** -3
+    pa_design = pa.design(freq, er, h)
+    pa.write_gerber_design(pa_design, "test1.gbr", feed_type="normal")
+    assert True
+
+
+def test_gerber_inset():
+    freq = 2.4 * 10 ** 9
+    er = 4.4
+    h = 1.6 * 10 ** -3
+    pa_design = pa.design(freq, er, h)
+    pa.write_gerber_design(pa_design, "test1.gbr", feed_type="inset")
+    assert True
